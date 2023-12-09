3a1fd8e2753c9fd0df81b56173a8611c078450df
MagePro310 <102444570+MagePro310@users.noreply.github.com>
2023-12-10 05:28:13 +0700

    Update auto_mode.h

--- Core/Inc/auto_mode.h
+++ Core/Inc/auto_mode.h
@@ -16,10 +16,8 @@
 #define VERTI 	1
 
 extern int led_time[3];
-extern int led_time_buffer[3];
 extern int horizontal_countdown;
 extern int vertical_countdown;
-extern int mode_and_time;
 
 enum LightState {Init, Red_Green, Red_Yellow, Green_Red, Yellow_Red};
 extern enum LightState light_state;
