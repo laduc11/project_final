b1c12957650fc31e9181357eff82b8a45b1989d8
La Minh Duc <laminhduc200@gmail.com>
2023-12-09 23:06:01 +0700

    update software timer

--- Core/Inc/software_timer.h
+++ Core/Inc/software_timer.h
@@ -8,6 +8,12 @@
 #ifndef INC_SOFTWARE_TIMER_H_
 #define INC_SOFTWARE_TIMER_H_
 
+#include "main.h"
 
+#define MAX_TIMER 5
+
+int get_flag(int idx_timer);
+void run_timer(int idx);
+void set_timer(int idx_timer, int duration);
 
 #endif /* INC_SOFTWARE_TIMER_H_ */
