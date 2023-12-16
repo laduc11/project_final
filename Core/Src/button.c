db8d33f44f90a388a1d6522d7cc6eed29ae84f35
laduc11 <117495540+laduc11@users.noreply.github.com>
2023-12-16 21:15:52 +0700

    Update button.c

--- Core/Src/button.c
+++ Core/Src/button.c
@@ -8,140 +8,138 @@
 #include "button.h"
 
 // Time cycle = 10ms
-#define WAITING_TIME 200	// press and hold in 2 seconds
+#define PRESS_TIME 200	// press and hold in 2 seconds
 
 typedef struct
 {
-	GPIO_TypeDef *port;
-	uint16_t pin;
-} listButton;
+	GPIO_PinState bState[4];
+} arrState;
 
-// List of button
-listButton button[MAX_BUTTON];
-statusButton buttonFlag[MAX_BUTTON];
-statusButton previousStatus[MAX_BUTTON][3];
-uint16_t counter[MAX_BUTTON];
-uint8_t isPressed;
+int counter[MAX_BUTTON];
+uint8_t bFlag[MAX_BUTTON] = {0};
+uint8_t bLongFlag[MAX_BUTTON] = {0};
+arrState button[MAX_BUTTON];
 
-/*
- * Initial start value for all attribute
- * Input: none
- * Output: none
+/*Initial button state
+ * input: none
+ * output: none
  * */
 void init_button()
 {
-	button[0].port = BUTTON1_GPIO_Port;
-	button[0].pin = BUTTON1_Pin;
-	button[1].port = BUTTON2_GPIO_Port;
-	button[1].pin = BUTTON2_Pin;
-
-	isPressed = 0;
-
-	for (int i = 0; i < MAX_BUTTON; i++)
+	for (uint8_t i = 0; i < MAX_BUTTON; i++)
 	{
-		counter[i] = WAITING_TIME;
-		buttonFlag[i] = NORMAL;
-		for (int j = 0; j < 3; j++)
+		button[i].bState[0] = GPIO_PIN_SET;
+		button[i].bState[1] = GPIO_PIN_SET;
+		button[i].bState[2] = GPIO_PIN_SET;
+		button[i].bState[3] = GPIO_PIN_SET;
+
+		counter[i] = PRESS_TIME;
+	}
+}
+
+/*
+ * Check 3 nearest state of index button
+ * input: index of button
+ * output: state of button
+ * */
+uint8_t checkState(uint8_t idx)
+{
+	if (button[idx].bState[1] == GPIO_PIN_RESET && button[idx].bState[2] == GPIO_PIN_RESET && button[idx].bState[3] == GPIO_PIN_RESET)
+		return GPIO_PIN_RESET;
+	return GPIO_PIN_SET;
+}
+
+uint8_t isPressed(uint8_t idx)
+{
+	if (bFlag[idx] == 1)
+	{
+		bFlag[idx] = 0;
+		return 1;
+	}
+	return 0;
+}
+
+uint8_t isLongPressed(uint8_t idx)
+{
+	if (bLongFlag[idx] == 1)
+	{
+		bLongFlag[idx] = 0;
+		return 1;
+	}
+	return 0;
+}
+
+/*
+ * Get input key
+ * input: index of button
+ * output: none
+ * */
+void getKey(uint8_t idx)
+{
+	button[idx].bState[1] = button[idx].bState[2];
+	button[idx].bState[2] = button[idx].bState[3];
+	switch (idx)
+	{
+	case 0:
+		button[idx].bState[3] = HAL_GPIO_ReadPin(BUTTON1_GPIO_Port, BUTTON1_Pin);
+		break;
+	case 1:
+		button[idx].bState[3] = HAL_GPIO_ReadPin(BUTTON2_GPIO_Port, BUTTON2_Pin);
+		break;
+	case 2:
+		button[idx].bState[3] = HAL_GPIO_ReadPin(BUTTON3_GPIO_Port, BUTTON3_Pin);
+		break;
+	case 3:
+		button[idx].bState[3] = HAL_GPIO_ReadPin(BUTTON4_GPIO_Port, BUTTON4_Pin);
+		break;
+	default:
+		return;
+	}
+
+	if (checkState(idx) == RESET)
+	{
+		if (button[idx].bState[0] == GPIO_PIN_SET)
 		{
-			previousStatus[i][j] = NORMAL;
+			// Debounced button
+			bFlag[idx] = 1;
+			button[idx].bState[0] = GPIO_PIN_RESET;
+			counter[idx] = PRESS_TIME;
+		}
+		else
+		{
+			// Long pressed button
+			if (counter[idx] == 0)
+			{
+				bLongFlag[idx] = 1;
+				counter[idx] = PRESS_TIME;
+			}
+			else
+			{
+				counter[idx]--;
+			}
 		}
 	}
+	else
+	{
+		button[idx].bState[0] = GPIO_PIN_SET;
+		counter[idx] = PRESS_TIME;
+	}
 }
 
-/*
- * Get input signal from all button
- * Input: none
- * Output: none
- * */
 void get_key()
 {
-	for (int i = 0; i < MAX_BUTTON; i++)
-	{
-		previousStatus[i][0] = previousStatus[i][1];
-		previousStatus[i][1] = previousStatus[i][2];
-		previousStatus[i][2] = (HAL_GPIO_ReadPin(button[i].port, button[i].pin) == GPIO_PIN_RESET) ? PRESSED : NORMAL;
-	}
-
-	// Process key
-	for (int i = 0; i < MAX_BUTTON; i++)
-	{
-        if ((previousStatus[i][0] == PRESSED) &&
-            (previousStatus[i][1] == PRESSED) &&
-            (previousStatus[i][2] == PRESSED))
-        {
-            if (isPressed)
-            {
-            	counter[i]--;
-            	if (counter[i] == 0)
-            	{
-            		counter[i] = WAITING_TIME;
-            		buttonFlag[i] = LONG_PRESSED;
-            	}
-            }
-            else
-            {
-            	isPressed = 1;
-            	buttonFlag[i] = PRESSED;
-            }
-        }
-        else
-        {
-        	isPressed = 0;
-        	counter[i] = WAITING_TIME;
-        	buttonFlag[i] = NORMAL;
-        }
-	}
+	getKey(0);
+	getKey(1);
+	getKey(2);
+	getKey(3);
 }
 
-/*
- * Process 4 nearest input read from button
- * Input: none
- * Output: none
- * */
-void processKey()
-{
-	for (int i = 0; i < MAX_BUTTON; i++)
-	{
-        if ((previousStatus[i][0] == PRESSED) &&
-            (previousStatus[i][1] == PRESSED) &&
-            (previousStatus[i][2] == PRESSED))
-        {
-            if (isPressed)
-            {
-            	counter[i]--;
-            	if (counter[i] == 0)
-            	{
-            		counter[i] = WAITING_TIME;
-            		buttonFlag[i] = LONG_PRESSED;
-            	}
-            }
-            else
-            {
-            	isPressed = 1;
-            	buttonFlag[i] = PRESSED;
-            }
-        }
-        else
-        {
-        	isPressed = 0;
-        	counter[i] = WAITING_TIME;
-        	buttonFlag[i] = NORMAL;
-        }
-	}
-}
-
-/*
- * Get status of chosen button
- * Input: Index of button
- * Output: Status of button
- * */
 statusButton get_button_flag(uint8_t buttonIdx)
 {
-	statusButton temp = buttonFlag[buttonIdx];
-	if (temp != NORMAL)
-	{
-		isPressed = 1;
-	}
-	buttonFlag[buttonIdx] = NORMAL;
-	return temp;
+	if (isLongPressed(buttonIdx))
+		return LONG_PRESSED;
+	else if (isPressed(buttonIdx))
+		return PRESSED;
+	else
+		return NORMAL;
 }
